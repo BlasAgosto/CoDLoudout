9ff1375a0264388ebcbf26c82c7e6b690f10ce2d
Blas Agosto Agosto <blasagosto@live.com>
2023-02-13 11:52:23 -0600

    added all loadouts

--- src/Components/images.js
+++ src/Components/images.js
@@ -3,11 +3,100 @@ import MCPR from '../Pics/mcpr300.png'
 import SIGNAL from '../Pics/signal50.PNG'
 import SPX from '../Pics/spx80.PNG'
 import VICTUS from '../Pics/victusxmr.png'
+import GS50 from '../Pics/.50gs.png'
+import Icarus556 from '../Pics/556icarus.png'
+import Basilisk from '../Pics/BAsilisk.png'
+import BASP from '../Pics/basp.png'
+import Bryson800 from '../Pics/bryson800.png'
+import Bryson890 from '../Pics/bryson890.png'
+import Chimera from '../Pics/chimera.png'
+import Erb14 from '../Pics/erb.png'
+import Expidite12 from '../Pics/expidite.png'
+import Fennec45 from '../Pics/fennec45.png'
+import FssHuricane from '../Pics/fsshuricane.png'
+import FtacRecon from '../Pics/ftacrecon.png'
+import HRC56 from '../Pics/hcr56.png'
+import Kastov74u from '../Pics/kastov74u.png'
+import Kastov545 from '../Pics/kastov545.png'
+import Kastov762 from '../Pics/kastov762.png'
+import Lachmann556 from '../Pics/lachamann556.png'
+import Lachmann762 from '../Pics/lachmann762.png'
+import Lachmannsub from '../Pics/lachmannsub.png'
+import LMS from '../Pics/LMS.png'
+import Lockwood300 from '../Pics/lockwood300.png'
+import LockwoodMk2 from '../Pics/lockwoodmk2.png'
+import M4 from '../Pics/M4.png'
+import M13B from '../Pics/m13b.png'
+import M16 from '../Pics/m16.png'
+import Minibank from '../Pics/minibank.png'
+import MX9 from '../Pics/mx9.png'
+import P890 from '../Pics/p890.png'
+import PDSW528 from '../Pics/pdsw528.png'
+import RAALMG from '../Pics/raalmg.png'
+import RAPPH from '../Pics/rapph.png'
+import RPK from '../Pics/rpk.png'
+import SAB50 from '../Pics/SAB50.png'
+import SakinMG38 from '../Pics/sakinmg38.png'
+import SO14 from '../Pics/so14.png'
+import SPR208 from '../Pics/spr208.png'
+import STB556 from '../Pics/stb556.png'
+import TAQ56 from '../Pics/taq56.png'
+import TAQM from '../Pics/TAQM.png'
+import TAQV from '../Pics/taqv.png'
+import Vaznev9K from '../Pics/vaznev9k.png'
+import VRL46 from '../Pics/vrl46.png'
+import X12 from '../Pics/x12.png'
+import X13 from '../Pics/x13auto.png'
 
 export default [
     LSB,
     MCPR,
     SIGNAL,
     SPX,
-    VICTUS
+    VICTUS,
+    GS50,
+    Icarus556,
+    BASP,
+    Basilisk,
+    Bryson800,
+    Bryson890,
+    Chimera,
+    Erb14,
+    Expidite12,
+    Fennec45,
+    Fennec45,
+    FssHuricane,
+    FtacRecon,
+    HRC56,
+    Kastov74u,
+    Kastov545,
+    Kastov762,
+    Lachmann556,
+    Lachmann762,
+    Lachmannsub,
+    LMS,
+    Lockwood300,
+    LockwoodMk2,
+    M4,
+    M13B,
+    M16,
+    Minibank,
+    MX9,
+    P890,
+    PDSW528,
+    RAALMG,
+    RAPPH,
+    RPK,
+    SAB50,
+    SakinMG38,
+    SO14,
+    SPR208,
+    STB556,
+    TAQ56,
+    TAQM,
+    TAQV,
+    Vaznev9K,
+    VRL46,
+    X12,
+    X13,
 ]
